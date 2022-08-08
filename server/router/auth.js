0cb3bde36c0fe49cf92987003de283c8ad5ab0dc
shyamsutar <shyamsutar629@gmail.com>
2022-08-08 20:27:41 +0530

    post.Auth

--- server/router/auth.js
+++ server/router/auth.js
@@ -4,7 +4,7 @@ const router = express.Router();
 require('../db/conn');
 const User = require('../model/userSchema');
 
-router.post('/register', (req, res) => {
+router.post('/register', async(req, res) => {
     // console.log(req.body);
     // res.json({message: req.body});
     const {name, email, phone, work, password, cpassword} = req.body;
@@ -13,21 +13,43 @@ router.post('/register', (req, res) => {
         return res.status(422).json({error: "Please fill the fields"});
     }
 
-    User.findOne({email: email}).then((userExist)=>{
+    // User.findOne({email: email}).then((userExist)=>{
+    //     if(userExist){
+    //         return res.status(422).json({error: "Email already exists"});
+    //     }
+
+    //     const user = new User({name, email, phone, work, password, cpassword});
+    //     user.save().then(()=>{
+    //         res.status(201).json({message: "user registered successfully"});
+    //     }).catch((e)=>{
+    //         res.status(500).json({error: "Failed to register"});
+    //     })
+
+    // }).catch((err)=>{
+    //     console.log(err);
+    // })
+
+
+    try {
+        const userExist = await User.findOne({email: email})
+        
+        
         if(userExist){
             return res.status(422).json({error: "Email already exists"});
         }
 
         const user = new User({name, email, phone, work, password, cpassword});
-        user.save().then(()=>{
-            res.status(201).json({message: "user registered successfully"});
-        }).catch((e)=>{
-            res.status(500).json({error: "Failed to register"});
-        })
 
-    }).catch((err)=>{
-        console.log(err);
-    })
+            const userRegister = await user.save();
+
+            if(userRegister){
+                res.status(201).json({message: "user registered successfully"});
+            }
+    
+    } catch (error) {
+        console.log(error);
+    }
+
 
   })
 
