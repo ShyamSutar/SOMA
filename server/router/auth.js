768e551809c96528e23fe95171655a8fa653f99f
shyamsutar <shyamsutar629@gmail.com>
2022-08-09 18:53:24 +0530

    jwt

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 require('../db/conn');
 const User = require('../model/userSchema');
@@ -60,6 +61,7 @@ router.post('/register', async(req, res) => {
   })
 
   router.post('/signin', async(req, res)=>{
+      let token;
       try {
         const {email, password} = req.body;
 
@@ -69,8 +71,13 @@ router.post('/register', async(req, res) => {
 
         const userLogin = await User.findOne({email: email});
 
+
+        const token = await userLogin.generateAuthToken();
+
+        console.log(token);
+
         if(userLogin){
-            
+
         const isMatch = await bcrypt.compare(password, userLogin.password);
 
             if(!isMatch){
