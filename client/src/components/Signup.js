8ecd28db3f9806ce561599f19de44bb68e695b4e
shyamsutar <shyamsutar629@gmail.com>
2022-08-14 20:19:04 +0530

    registration page

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,7 +1,174 @@
-import React from 'react'
+import React from "react";
+import registration from "../images/registration.png";
+import {Link } from "react-router-dom";
 
 export default function Signup() {
   return (
-    <div>Signup</div>
-  )
+    <>
+      <section className="signup mt-16 ">
+        {/* <div className='container mx-auto mt-5 shadow'> */}
+
+        <div className="">
+          <div class="p-2 w-full bg-white rounded-lg shadow-xl dark:border md:mt-0  sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 container mx-auto flex justify-around flex-wrap items-center ">
+            <div className="left ">
+              {/* <h2 className="p-6 sm:p-8 md:space-y-6 text-3xl font-bold">SIGN UP</h2> */}
+
+              <div class="p-1 space-y-4 md:space-y-6 sm:p-1">
+                <h1 class="text-3xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  Sign Up
+                </h1>
+                <form class="space-y-4 md:space-y-6" action="#">
+                  <div>
+                    <label
+                      for="name"
+                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Your name
+                    </label>
+                    <input
+                      type="text"
+                      name="text"
+                      id="text"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="John"
+                      required=""
+                    />
+                  </div>
+
+                  <div>
+                    <label
+                      for="email"
+                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Your email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="name@company.com"
+                      required=""
+                    />
+                  </div>
+
+                  <div>
+                    <label
+                      for="number"
+                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Your Mobile No
+                    </label>
+                    <input
+                      type="tel"
+                      name="mobile"
+                      id="mobile"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="1234567899"
+                      required=""
+                    />
+                  </div>
+
+                  <div>
+                    <label
+                      for="profession"
+                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Your profession
+                    </label>
+                    <input
+                      type="text"
+                      name="profession"
+                      id="profession"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="Developer"
+                      required=""
+                    />
+                  </div>
+
+                  <div>
+                    <label
+                      for="password"
+                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="••••••••"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      required=""
+                    />
+                  </div>
+                  <div>
+                    <label
+                      for="confirm-password"
+                      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >
+                      Confirm password
+                    </label>
+                    <input
+                      type="confirm-password"
+                      name="confirm-password"
+                      id="confirm-password"
+                      placeholder="••••••••"
+                      class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      required=""
+                    />
+                  </div>
+                  {/* <div class="flex items-start"> */}
+                    {/* <div class="flex items-center h-5">
+                      <input
+                        id="terms"
+                        aria-describedby="terms"
+                        type="checkbox"
+                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+                        required=""
+                      />
+                    </div> */}
+                    {/* <div class="ml-3 text-sm">
+                      <label
+                        for="terms"
+                        class="font-light text-gray-500 dark:text-gray-300"
+                      >
+                        I accept the{" "}
+                        <a
+                          class="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                          href="/"
+                        >
+                          Terms and Conditions
+                        </a>
+                      </label>
+                    </div> */}
+                  {/* </div> */}
+                  <button
+                    type="submit"
+                    class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Create an account
+                  </button>
+                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                    Already have an account?{" "}
+                    <Link
+                      to="/login"
+                      class="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                    >
+                      Login here
+                    </Link>
+                  </p>
+                </form>
+              </div>
+            </div>
+            <div className="right">
+              <figure>
+                <img src={registration} alt="registration image" />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
