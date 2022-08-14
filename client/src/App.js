25759be945a2f68306d4016a1e51090eb1b37aa0
shyamsutar <shyamsutar629@gmail.com>
2022-08-14 13:31:17 +0530

    react-router-dom added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+// import{Route, Routes} from 'react-router-dom';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+// import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
-    <div>Hello world</div>
-  )
+    <Router>
+      <>
+        <Navbar />
+
+        <Routes>
+          <Route exact path="home" element={<Home/>} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<Signup />} />
+        </Routes>
+      </>
+    </Router>
+  );
 }
