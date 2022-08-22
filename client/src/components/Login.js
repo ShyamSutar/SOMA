73829bdb5b3682267323afaf7deecd87a4427f26
shyamsutar <shyamsutar629@gmail.com>
2022-08-22 21:13:03 +0530

    fixing bug

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import loginImage from "../images/login.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Login() {
+
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loginUser = async(e) =>{
+      e.preventDefault();
+
+      const res = await fetch('/signin', {
+        method : "POST",
+        headers : {
+          "Content-Type": "application/json"
+        },
+        body:JSON.stringify({
+          email, password
+        })
+      })
+
+      const data = res.json();
+
+      if(data.status === 400 || !data){
+        window.alert('invalid credentials');
+      }else{
+        window.alert("login success")
+        navigate('/')
+      }
+
+  }
+
   return (
     <div>
       <section className="signup p-12 h-screen">
@@ -13,10 +42,10 @@ export default function Login() {
                 <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                   Sign in to your account
                 </h1>
-                <form className="space-y-4 md:space-y-6" action="#">
+                <form className="space-y-4 md:space-y-6" method="POST">
                   <div>
                     <label
-                      for="email"
+                      htmlFor="email"
                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                     >
                       Your email
@@ -27,12 +56,14 @@ export default function Login() {
                       id="email"
                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       placeholder="name@company.com"
+                      value={email}
+                      onChange={(e)=>setEmail(e.target.value)}
                       required=""
                     />
                   </div>
                   <div>
                     <label
-                      for="password"
+                      htmlFor="password"
                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                     >
                       Password
@@ -43,12 +74,15 @@ export default function Login() {
                       id="password"
                       placeholder="••••••••"
                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      value={password}
+                      onChange={(e)=>setPassword(e.target.value)}
                       required=""
                     />
                   </div>
                   
                   <button
                     type="submit"
+                    onClick={loginUser}
                     className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                   >
                     Sign in
